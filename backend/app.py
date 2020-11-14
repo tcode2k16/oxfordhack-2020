96bde432d36e5c90b7d62b67a02ee96631a00644
JackBai <xingjianbai0914@sina.com>
2020-11-14 21:43:38 +0000

    Xingjian's first comment

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,11 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+  # identity info
   id = db.Column(db.Integer, primary_key=True)
+  password = db.Column(db.String(500), nullable=False)
+  
+  # personal info
   name = db.Column(db.String(100), nullable=False)
   college = db.Column(db.String(100), nullable=False)
   department = db.Column(db.String(100), nullable=False)
@@ -19,30 +23,42 @@ class User(db.Model):
   gender = db.Column(db.String(100), nullable=False)
   pronouns = db.Column(db.String(100), nullable=False)
   description = db.Column(db.String(500), nullable=False)
-  password = db.Column(db.String(500), nullable=False)
 
   def __repr__(self):
     return '<User %r>' % self.name
 
-class Task(db.Model):
+class Hangout(db.Model):
+  #identity
   id = db.Column(db.Integer, primary_key=True)
-  time = db.Column(db.String(100), nullable=False)
+
+  #status
+  status = db.Column(db.String(100), nullable=False) #available, matched, finalized, finished
+
+  #user1
+  author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  author = db.relationship('User', backref=db.backref('hangouts', lazy=True), lazy=True)
+
+  #hard condition
+  cond_name = db.Column(db.String(100), nullable=False) # *
+  cond_college = db.Column(db.String(100), nullable=False) # *
+  cond_department = db.Column(db.String(100), nullable=False) # *
+  cond_year = db.Column(db.Integer, nullable=False) # 0
+  cond_gender = db.Column(db.String(100), nullable=False) # *
+
+  #soft condition
   activity = db.Column(db.String(100), nullable=False)
+  time = db.Column(db.String(100), nullable=False)
   location = db.Column(db.String(100), nullable=False)
   
-  author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-  author = db.relationship('User', backref=db.backref('tasks', lazy=True))
   
   def __repr__(self):
-    return '<Task %r %r %r>' % (self.time, self.activity, self.location)
-
+    return '<Hangout %r %r %r>' % (self.time, self.activity, self.location)
 
 db.create_all()
 
 @app.route('/')
 def entry():
   return send_from_directory('../static', './index.html')
-
 
 # @app.route('/<path:path>')
 # def send_files(path):
@@ -51,4 +67,4 @@ def entry():
 import routes.auth
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(port = 8000, debug=True)
--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -3,20 +3,18 @@ from markupsafe import escape
 import bcrypt
 from app import app, User, db
 
-
 @app.route('/auth')
 def index():
   if 'user_id' in session:
     return 'Logged in as %s' % escape(session['user_id'])
   return 'You are not logged in'
 
+# User Registration
 @app.route('/auth/register', methods=['POST'])
 def register():
-  
   if request.json == None or not all([x in request.json for x in ['name','college','department','email','year','phone_number','gender','pronouns','description','password']]):
     return jsonify({'error': 'missing info'})
   
-
   u = User(
     name=request.json['name'],
     college=request.json['college'],
@@ -38,11 +36,11 @@ def register():
 
   return jsonify({'status': 'success'})
 
-
-
-
+# User Login
 @app.route('/auth/login', methods=['POST'])
 def login():
+  if request.json == None or not all([x in request.json for x in ['email', 'password']]):
+    return jsonify({'error': 'missing login info'})
   email = request.json['email']
   password = request.json['password']
 
@@ -55,11 +53,89 @@ def login():
     return jsonify({'error': 'wrong password'})
   
   session['user_id'] = user.id
-  
   return jsonify({'status': 'success'})
-
 
 @app.route('/auth/logout')
 def logout():
   session.pop('user_id', None)
   return redirect("/")
+
+#get all (available/matched/finalized/finished) hangouts I posted
+@app.route('/auth/my_hangouts')
+def my_hangouts():
+  hangout_type = request.json['hangout_type']
+  author_id = session['user_id']
+  #***
+  hangouts = Hangouts.query.filter_by(author_id = author_id, status = 'hangout_type').all()
+  return jsonify({'hangouts': hangouts})
+
+#get all available hangouts that fit me
+@app.route('/auth/my_feed')
+def my_feed():
+  uid = session['user_id']
+  user = User.query.filter_by(id=uid).first()
+  name = user.name
+  college = user.college
+  department = user.department
+  year = user.year
+  gender = user.gender
+
+  hangouts = Hangouts.query.all()
+  feeds = []
+  for hangout in hangouts:
+    if name == hangout.cond_name and \
+       college == hangout.cond_college and \
+       department == hangout.department and \
+       year == hangout.year and \
+       gender == hangout.gender:
+       feeds.append(hangout)
+
+  return jsonify({'feeds': feeds})
+
+
+#1. publish hangout
+@app.route('/auth/publish', methods=['POST'])
+def publish():
+  if request.json == None or not all([x in request.json for x in ['time','location','activity','cond_name','cond_college','cond_department','cond_gender','cond_year']]):
+    return jsonify({'error': 'missing info'})
+  
+  h = Hangout(
+    time=request.json['time'],
+    location=request.json['location'],
+    activity=request.json['activity'],
+    cond_name=request.json['cond_name'],
+    cond_college=request.json['cond_college'],
+    cond_department=request.json['cond_department'],
+    cond_gender=request.json['cond_gender'],
+    cond_year=request.json['cond_year'],
+    status='available'
+  )
+  db.session.add(h)
+  db.session.commit()
+  print(h.id)
+  session['hangout_id'] = h.id
+
+  return jsonify({'status': 'success'})
+
+#2. match a hangout
+# @app.route('/auth/match')
+# def match():
+#   uid = session['user_id']
+#   user = User.query.filter_by(id=uid).first()
+#   hid = request.json['hid']
+
+  
+
+#   hangouts = Hangouts.query.all()
+#   feeds = []
+#   for hangout in hangouts:
+#     if name == hangout.cond_name and \
+#        college == hangout.cond_college and \
+#        department == hangout.department and \
+#        year == hangout.year and \
+#        gender = hangout.gender:
+#        feeds.append(hangout)
+
+#   session['my_feeds'] = feeds
+
+#   return jsonify({'status': 'success'})
